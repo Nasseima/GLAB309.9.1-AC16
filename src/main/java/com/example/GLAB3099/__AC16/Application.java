eb898246a3c36317eac426eaafb554b217ea5e32
Nasseima <lindsaynasseima@gmail.com>
2024-08-23 09:03:31 -0400

    All done with my file upload page

---
+++ src/main/java/com/example/GLAB3099/__AC16/Application.java
@@ -0,0 +1,13 @@
+package com.example.GLAB3099.__AC16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
